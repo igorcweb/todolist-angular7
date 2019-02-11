3d2c0b5060a2c201bb32e57d9aaf6b046dbdda7a
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2019-02-10 20:35:07 -0600

    events, services

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -22,4 +22,11 @@ export class TodoItemComponent implements OnInit {
 
     return classes;
   }
+  onToggle(todo) {
+    todo.completed = !todo.completed;
+  }
+
+  onDelete(todo) {
+    console.log('delete')
+  }
 }
